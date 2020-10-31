06e67694d24a6c498d2fb8e1af8f721d60b5a92e
Oana Surdea <P3701260@ness.com>
2020-10-31 14:16:11 +0200

    Updated the app component + tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fireship-angular-testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('fireship-angular-testing');
-  });
-
-  it('should render title', () => {
+  it('should render child component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fireship-angular-testing app is running!');
+    expect(compiled.querySelectorAll('app-alert-button').length).toBeGreaterThanOrEqual(1);
   });
 });
